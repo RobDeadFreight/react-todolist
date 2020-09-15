7d3047e1b87fbb78bbdf52212dde69cd17739477
RobDeadFreight <robertfranklindesign@gmail.com>
2020-09-15 01:17:23 +0100

    Built App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+// Importing Components
+import Form from './components/Form';
+import TodoList from "./components/TodoList";
 
 function App() {
+
+  // State
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  // Run once
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  // Use Effect
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  // functions
+  const filterHandler = () => {
+    switch(status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break
+      default: 
+        setFilteredTodos(todos);
+    }
+  }
+
+  // Save to Local Storage
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
+  const getLocalTodos = () => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"))
+      setTodos(todoLocal);
+    }
+  }
+
   return (
     <div className="app">
-      <h1>Hello react</h1>
-      <h2>test</h2>
+      <header>
+        <h1>Rob's Todo List</h1>
+      </header>
+      <Form 
+        todos={todos}
+        setTodos={setTodos} 
+        setInputText={setInputText} 
+        inputText={inputText}
+        status={status}
+        setStatus={setStatus}
+        />
+      <TodoList
+        setTodos={setTodos} 
+        todos={todos} 
+        filteredTodos={filteredTodos}
+        inputText={inputText}
+        />
     </div>
   );
 }
